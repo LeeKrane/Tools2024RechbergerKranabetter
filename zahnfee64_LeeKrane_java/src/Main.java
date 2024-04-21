0af6fbe81817530b99ddd9152cfdb7699abbf454
krane <christian.kranabetter@protonmail.com>
2024-04-21 20:21:58 +0200

    add some LoC

--- zahnfee64_LeeKrane_java/src/Main.java
+++ zahnfee64_LeeKrane_java/src/Main.java
@@ -7,5 +7,17 @@ public class Main {
 	public void thisIsANewMethod () {
 		System.out.println("Mina's change");
 		System.out.println("Krane's change!");
+		int uselessVar = 10;
+		System.out.println(uselessVar);
+		System.out.println(add(uselessVar, 20));
+		System.out.println(mul(uselessVar, 20));
+	}
+	
+	private int add (int originalVar, int addition) {
+		return originalVar + addition;
+	}
+	
+	private int mul (int originalVar, int multiplicator) {
+		return originalVar * multiplicator;
 	}
 }
